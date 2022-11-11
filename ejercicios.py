4649aba76756686ea870ff27daafa318bd0c9203
munilla1 <mucho-muni@hotmail.com>
2022-11-11 16:46:48 +0100

    ejercicio 1 resulto

---
+++ ejercicios.py
@@ -0,0 +1,7 @@
+# programa que, recibe dos numeros enteros y calcula la suma, la resta y la multiplicacion de dichos numeros
+
+def suma(primer_numero, segundo_numero):
+    resultado = primer_numero + segundo_numero
+    return resultado
+
+print(suma(4, 5))
